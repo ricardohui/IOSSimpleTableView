0a75bb79f85ac670dd218654739f25229856f074
Ricardo <eehaha@gmail.com>
2019-03-02 12:26:02 +0800

    minimalist working table view

--- SimpleTableView/ViewController.swift
+++ SimpleTableView/ViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     return 10
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default , reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Top row"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
